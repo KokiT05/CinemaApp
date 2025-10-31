437655ec7c47f71b1e869baceb71e91507496e51
Konstantin Tinev <konstantintinev5@gmail.com>
2025-10-31 18:00:17 +0200

    Add new Movie model with configuration and seed data

---
+++ CinemaApp.Data.Models/Movie.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApp.Data.Models
+{
+    [Comment("Movie in the system")]
+    public class Movie
+    {
+        [Comment("Movie Identifier")]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Comment("Movie Title")]
+        public string Title { get; set; } = null!;
+
+        [Comment("Movie Genre")]
+        public string Genre { get; set; } = null!;
+
+        [Comment("Movie Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Comment("Movie Director")]
+        public string Director { get; set; } = null!;
+
+        [Comment("Movie Duration")]
+        public int Duration { get; set; }
+
+        [Comment("Movie Description")]
+        public string Description { get; set; } = null!;
+
+        [Comment("Movie Image Url")]
+        public string? ImageUrl { get; set; }
+
+        [Comment("Movie Is Deleted")]
+        public bool IsDeleted { get; set; } = false;
+    }
+}
--- CinemaApp.Data/ApplicationDbContext.cs
+++ CinemaApp.Data/ApplicationDbContext.cs
@@ -1,12 +1,24 @@
 ﻿namespace CinemaApp.Data
 {
+    using CinemaApp.Data.Models;
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
+    using System.Reflection;
+
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
     }
 }
