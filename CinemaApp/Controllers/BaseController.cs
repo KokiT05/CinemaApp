9d238add927cda0424c1ae76bdf256da1425b430
Konstantin Tinev <konstantintinev5@gmail.com>
2025-11-21 23:48:21 +0200

    Modify Movie Index page and add constructor to WatchlistController

--- CinemaApp/Controllers/BaseController.cs
+++ CinemaApp/Controllers/BaseController.cs
@@ -1,11 +1,32 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace CinemaApp.Web.Controllers
 {
     [Authorize]
     public class BaseController : Controller
     {
+        protected bool IsUserAuthenticated()
+        {
+            if (this.User == null)
+            {
+                return false;
+            }
+
+            if (this.User.Identity == null)
+            {
+                return false;
+            }
+
+            return this.User.Identity!.IsAuthenticated;
+        }
+
+        protected string GetUserId()
+        {
+            return this.User.FindFirstValue(ClaimTypes.NameIdentifier) ?? string.Empty;
+        }
+
         public IActionResult Index()
         {
             return View();
