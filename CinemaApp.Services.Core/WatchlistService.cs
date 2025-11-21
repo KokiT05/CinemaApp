ee1e5b8a5d1649ae8ae68e4da38978a02028e239
Konstantin Tinev <konstantintinev5@gmail.com>
2025-11-21 22:00:19 +0200

    Add UserMovie model with configuration and Watchlist feature + Authorize

---
+++ CinemaApp.Services.Core/WatchlistService.cs
@@ -0,0 +1,50 @@
+﻿using CinemaApp.Data;
+using CinemaApp.Data.Models;
+using CinemaApp.Services.Core.Interfaces;
+using CinemaApp.Web.ViewModels.Watchlist;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApp.Services.Core
+{
+    using static GCommon.ApplicationConstants;
+    public class WatchlistService : IWatchlistService
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public WatchlistService(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<WatchlistViewModel>> GetUserWatchlistAsync(string userId)
+        {
+            List<WatchlistViewModel> userWatchlist = new List<WatchlistViewModel>();
+
+            bool isValidId = Guid.TryParse(userId, out Guid id);
+
+            if (isValidId)
+            {
+                userWatchlist = await this.dbContext.UserMovies
+                                            .Include(um => um.Movie)
+                                            .AsNoTracking()
+                                            .Where(um => um.UserId == userId)
+                                            .Select(um => new WatchlistViewModel()
+                                            {
+                                                MovieId = um.MovieId.ToString(),
+                                                Title = um.Movie.Title,
+                                                Genre = um.Movie.Genre,
+                                                ReleaseDate = um.Movie.ReleaseDate
+                                                                .ToString(ApplicationDateFormat),
+                                                ImageUrl = um.Movie.ImageUrl,
+                                            }).ToListAsync();
+            }
+
+            return userWatchlist;
+        }
+    }
+}
---
+++ CinemaApp/Controllers/WatchlistController.cs
@@ -0,0 +1,26 @@
+﻿using CinemaApp.Web.ViewModels.Watchlist;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaApp.Web.Controllers
+{
+    public class WatchlistController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return this.View(new List<WatchlistViewModel>());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            return this.View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Remove()
+        {
+            return this.View();
+        }
+    }
+}
