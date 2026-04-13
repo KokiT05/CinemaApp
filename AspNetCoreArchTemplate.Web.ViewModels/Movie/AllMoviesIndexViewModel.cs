f86a072b8db38cdeda848a7f974290104975aa35
Konstantin Tinev <konstantintinev5@gmail.com>
2026-04-13 22:09:17 +0300

    Add MovieController, Index view, and AllMoviesIndexViewModel

---
+++ AspNetCoreArchTemplate.Web.ViewModels/Movie/AllMoviesIndexViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCoreArchTemplate.Web.ViewModels.Movie
+{
+	public class AllMoviesIndexViewModel
+	{
+		//
+		public string Id { get; set; } = null!;
+
+		public string Title { get; set; } = null!;
+
+		public string Genre	{ get; set; } = null!;
+
+		public string ReleaseDate { get; set; } = null!;
+
+		public string Director { get; set; } = null!;
+
+		public string Duration	{ get; set; } = null!;
+
+		public string? ImageUrl { get; set; }
+	}
+}
---
+++ AspNetCoreArchTemplate.Web/Controllers/MovieController.cs
@@ -0,0 +1,31 @@
+﻿using AspNetCoreArchTemplate.Data;
+using AspNetCoreArchTemplate.Web.ViewModels.Movie;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreArchTemplate.Web.Controllers
+{
+	public class MovieController : Controller
+	{
+		private readonly ApplicationDbContext context;
+
+		public MovieController(ApplicationDbContext context)
+		{
+			this.context = context;
+		}
+		public async Task<IActionResult> Index()
+		{
+			List<AllMoviesIndexViewModel> movies = await context.Movies.Select(m => new AllMoviesIndexViewModel
+														{
+															Id = m.Id.ToString(),
+															Title = m.Title,
+															Genre = m.Genre,
+															ReleaseDate = m.ReleaseDate.ToString("yyyy-MM-dd"),
+															Director = m.Director,
+															ImageUrl = m.ImageUrl
+														}).ToListAsync();
+
+			return View(movies);
+		}
+	}
+}
