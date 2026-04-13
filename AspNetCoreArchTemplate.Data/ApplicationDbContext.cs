ec95b397532312a7f53b4a5337f5f9c474080ac5
Konstantin Tinev <konstantintinev5@gmail.com>
2026-04-13 20:29:49 +0300

    Add Movie entity and seed data with migrations

--- AspNetCoreArchTemplate.Data/ApplicationDbContext.cs
+++ AspNetCoreArchTemplate.Data/ApplicationDbContext.cs
@@ -1,14 +1,25 @@
 ﻿namespace AspNetCoreArchTemplate.Data
 {
-    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+	using AspNetCoreArchTemplate.Data.Models;
+	using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
+	using System.Reflection;
 
-    public class ApplicationDbContext : IdentityDbContext
+	public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
 
         }
+
+        public virtual DbSet<Movie> Movies { get; set; } = null!;
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+		}
     }
 }
