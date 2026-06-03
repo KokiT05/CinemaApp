135869fb42c874efc29b3b9d89e0a2b406f71567
Konstantin Tinev <konstantintinev5@gmail.com>
2026-06-03 19:47:10 +0300

    Add Watchlist view, view model and controller

---
+++ CinemaApp.Web/Controllers/WatchlistController.cs
@@ -0,0 +1,25 @@
+﻿using CinemaApp.Web.ViewModels.Watchlist;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaApp.Web.Controllers
+{
+    public class WatchlistController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<WatchlistViewModel> watchlistViewModel = new List<WatchlistViewModel>();
+            return View(watchlistViewModel);
+        }
+
+        public IActionResult Add()
+        {
+            return this.RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove()
+        {
+            return this.RedirectToAction(nameof(Index));
+        }
+    }
+}
